f7705b649f4086f8f746dd20894a640c969d1ebb
Johnny <63070273+TJfiles@users.noreply.github.com>
2025-08-18 16:00:03 -0300

    Add files via upload

--- Ex_5.py
+++ Ex_5.py
@@ -1,12 +1,12 @@
 # Utilizando tanto um loop while quanto um loop for, escreva um código que exiba na tela o resultado abaixo:
 
-# 1 Fabrício
-# 2 Leandro
-# 3 Marcela
+#ID: 354 | Aluno: Fabrício
+#ID: 847 | Aluno: Leandro
+#ID: 195 | Aluno: Marcela
 
 
 # ------------------------------------------ ESCREVA SEU CÓDIGO ABAIXO -----------------------------------------------------------
-lista = ['Fabrício', 'Leandro', 'Marcela', [1, 2, 3, 0]] # Não apague e nem altere essa lista
+lista = ['Fabrício', 'Leandro', 'Marcela', [354, 847, 195, 0]] # Não apague e nem altere essa lista
 
 
 # LOOP WHILE
